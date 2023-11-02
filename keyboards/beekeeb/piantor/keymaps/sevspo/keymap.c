d24f22cc2b532f72b1b81e2362a108a2878b596c
Severin Spoerri <severin.spoerri@outlook.com>
2023-11-02 23:29:40 +0100

    keymap v10
    tabace for equals and comment

--- keyboards/beekeeb/piantor/keymaps/sevspo/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/sevspo/keymap.c
@@ -32,7 +32,9 @@ enum unicode_names {
 };
 
 enum tab_dance_names {
-  TD_CTRLS_C = 0,
+  TD_CTRLS_C,
+  TD_DASH_EQ,
+  TD_SLSH_CMM,
 };
 
 const uint32_t PROGMEM unicode_map[] = {
@@ -65,6 +67,8 @@ const uint32_t PROGMEM unicode_map[] = {
 
 tap_dance_action_t tap_dance_actions[] = {
   [TD_CTRLS_C] = ACTION_TAP_DANCE_DOUBLE(KC_F12, RCS(KC_C)),
+  [TD_DASH_EQ] = ACTION_TAP_DANCE_DOUBLE(KC_MINS, KC_EQL),
+  [TD_SLSH_CMM] = ACTION_TAP_DANCE_DOUBLE(KC_SLSH, RCTL(KC_SLSH)),
 };
 
 
@@ -85,16 +89,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       */
 
     [0] = LAYOUT_split_3x6_3(
-        KC_ESC,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_MINS,
+        KC_ESC,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           TD(TD_DASH_EQ),
         LCTL(KC_C),     LALT_T(KC_A),   LSFT_T(KC_S),   LCTL_T(KC_D),   LT(3,KC_F),     LT(1,KC_G),     LT(3,KC_H),     LT(1,KC_J),     RCTL_T(KC_K),   RSFT_T(KC_L),   RALT_T(KC_SCLN),KC_QUOT,
-        LCTL(KC_V),     KC_Z,           KC_X,           KC_C,           LT(2,KC_V),     LT(4,KC_B),     LT(4,KC_N),     LT(2,KC_M),     RGUI_T(KC_COMM),KC_DOT,         KC_SLSH,        KC_UNDS,
-                                                        TD(TD_CTRLS_C),         LCA(KC_N),      KC_TAB,         KC_ENT,         KC_SPC,         KC_BSPC
+        LCTL(KC_V),     KC_Z,           KC_X,           LGUI_T(KC_C),   LT(2,KC_V),     LT(4,KC_B),     LT(4,KC_N),     LT(2,KC_M),     RGUI_T(KC_COMM),KC_DOT,         TD(TD_SLSH_CMM),KC_UNDS,
+                                                        TD(TD_CTRLS_C), LCA(KC_N),      KC_TAB,         KC_ENT,         KC_SPC,         KC_BSPC
     ),
 
     [1] = LAYOUT_split_3x6_3(
-        KC_ESC,         KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_LBRC,        KC_7,           KC_8,          KC_9,            KC_EQL,         KC_MINS,
-        KC_TRNS,        LALT_T(KC_F6),  LSFT_T(KC_F7),  LCTL_T(KC_F8),  KC_F9,          KC_F10,         KC_RBRC,        KC_4,           RCTL_T(KC_5),  RSFT_T(KC_6),    RALT_T(KC_GRV),   KC_BSLS,
-        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_F10,         KC_F12,         KC_0,           KC_1,           KC_2,          KC_3,            KC_SLSH,        KC_UNDS,
+        KC_ESC,         KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_NO,          KC_7,           KC_8,          KC_9,            KC_BSLS,        KC_TRNS,
+        KC_TRNS,        LALT_T(KC_F6),  LSFT_T(KC_F7),  LCTL_T(KC_F8),  KC_F9,          KC_F10,         KC_NO,          KC_4,           RCTL_T(KC_5),  RSFT_T(KC_6),    RALT_T(KC_GRV), KC_QUOT,
+        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_F11,         KC_F12,         KC_0,           KC_1,           KC_2,          KC_3,            KC_TRNS,        KC_UNDS,
                                                         KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_ENT,         KC_SPC,         KC_BSPC
     ),
 
@@ -108,14 +112,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [3] = LAYOUT_split_3x6_3(
         KC_ESC,         KC_BRIU,        KC_WSCH,        KC_WBAK,        KC_WFWD,        KC_WREF,        RCTL(KC_X),     KC_HOME,         KC_UP,         KC_END,         KC_PGUP,        KC_PPLS,
         KC_TRNS,        LALT_T(KC_BRID),LSFT_T(KC_TRNS),LCTL_T(KC_MUTE),KC_MPLY,        KC_VOLU,        RCTL(KC_C),     KC_LEFT,         KC_DOWN,       KC_RGHT,        KC_PGDN,        KC_PMNS,
-        KC_TRNS,        SGUI(KC_S),     LGUI(KC_E),     KC_MSTP,        KC_MFFD,        KC_VOLD,        RCTL(KC_V),     RCTL(KC_Z),      RCTL(KC_Y),    KC_APP,         KC_INS,          KC_DEL,
+        KC_TRNS,        SGUI(KC_S),     LGUI(KC_E),     KC_MSTP,        KC_MFFD,        KC_VOLD,        RCTL(KC_V),     RCTL(KC_Z),      RCTL(KC_Y),    KC_APP,         KC_TRNS,        KC_DEL,
                                                         KC_TRNS,        LCA_T(KC_TRNS), C_S_T(KC_TRNS), KC_ENT,         KC_SPC,          KC_BSPC
     ),
 
     [4] = LAYOUT_split_3x6_3(
-        UM(HEARNOEVIL),         UM(SKULL),       UM(ASTONISHED),  UM(HEART),    UM(ROBOT),      UM(HEXPLODE),     KC_NO,        DEBAE,            DEBOE,            DEBUE,            KC_NO,        KC_NO,
-        UM(SEENOEVIL),          UM(POOP),        UM(CRY),         UM(WINK),     UM(FISTBOX),    UM(EXPLODE),      KC_NO,        UP(DESAE, DEBAE), UP(DESOE, DEBOE), UP(DESUE, DEBUE), KC_E,         KC_NO,
-        UM(SPEAKNOEVIL),        KC_LSFT,         UM(FRIES),       UM(BUG),      UM(GRIMACE),    UM(SMILE),        KC_NO,        KC_NO,            KC_NO,            KC_NO,            KC_NO,        KC_NO,
-                                                                  TD(TD_CTRLS_C),       KC_NO,          KC_TAB,           KC_ENT,       KC_SPC,           KC_BSPC
+        UM(HEARNOEVIL),         UM(SKULL),       UM(ASTONISHED),  UM(HEART),      UM(ROBOT),      UM(HEXPLODE),     KC_NO,        UM(DEBAE),        UM(DEBOE),        UM(DEBUE),        KC_NO,        KC_NO,
+        UM(SEENOEVIL),          UM(POOP),        UM(CRY),         UM(WINK),       UM(FISTBOX),    UM(EXPLODE),      KC_NO,        UP(DESAE, DEBAE), UP(DESOE, DEBOE), UP(DESUE, DEBUE), KC_E,         KC_NO,
+        UM(SPEAKNOEVIL),        KC_LSFT,         UM(FRIES),       UM(BUG),        UM(GRIMACE),    UM(SMILE),        KC_NO,        KC_NO,            KC_NO,            KC_NO,            KC_NO,        KC_NO,
+                                                                  TD(TD_CTRLS_C), KC_NO,          KC_TAB,           KC_ENT,       KC_SPC,           KC_BSPC
     )
 };
