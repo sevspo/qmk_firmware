1b466b25389c742750e3743b544647380a8c468e
Severin Spoerri <severin.spoerri@outlook.com>
2025-08-11 17:47:37 +0200

    latest layout with lots of emojies

--- keyboards/beekeeb/piantor/keymaps/sevspo/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/sevspo/keymap.c
@@ -24,6 +24,11 @@ enum unicode_names {
     SKULL,
     EXPLODE,
     FISTBOX,
+    GRIMACE,
+    HEXPLODE,
+    ROBOT,
+    BUG,
+    FRIES,
 };
 
 const uint32_t PROGMEM unicode_map[] = {
@@ -47,6 +52,11 @@ const uint32_t PROGMEM unicode_map[] = {
     [SKULL] = 0x1F480,
     [EXPLODE] = 0x1F4A5,
     [FISTBOX] = 0x1F91C,
+    [GRIMACE] = 0x1F62C,
+    [HEXPLODE] = 0x1F92F,
+    [ROBOT] = 0x1F916,
+    [BUG] = 0x1F41B,
+    [FRIES] = 0x1F35F,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -67,36 +77,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_split_3x6_3(
         KC_ESC,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_MINS,
-        LCTL(KC_C),     LALT_T(KC_A),   LSFT_T(KC_S),   LCTL_T(KC_D),   LT(1,KC_F),     LT(3,KC_G),     LT(3,KC_H),     LT(1,KC_J),     RCTL_T(KC_K),   RSFT_T(KC_L),   RALT_T(KC_SCLN),KC_QUOT,
+        LCTL(KC_C),     LALT_T(KC_A),   LSFT_T(KC_S),   LCTL_T(KC_D),   LT(3,KC_F),     LT(1,KC_G),     LT(3,KC_H),     LT(1,KC_J),     RCTL_T(KC_K),   RSFT_T(KC_L),   RALT_T(KC_SCLN),KC_QUOT,
         LCTL(KC_V),     KC_Z,           KC_X,           KC_C,           LT(2,KC_V),     LT(4,KC_B),     LT(4,KC_N),     LT(2,KC_M),     RGUI_T(KC_COMM),KC_DOT,         KC_SLSH,        KC_UNDS,
-                                                        KC_F12,         KC_NO,          KC_TAB,         KC_ENT,         KC_SPC,         KC_BSPC
+                                                        KC_F12,         LCA(KC_N),      KC_TAB,         KC_ENT,         KC_SPC,         KC_BSPC
     ),
 
     [1] = LAYOUT_split_3x6_3(
         KC_ESC,         KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_LBRC,        KC_7,           KC_8,          KC_9,            KC_EQL,         KC_MINS,
         KC_TRNS,        LALT_T(KC_F6),  LSFT_T(KC_F7),  LCTL_T(KC_F8),  KC_F9,          KC_F10,         KC_RBRC,        KC_4,           RCTL_T(KC_5),  RSFT_T(KC_6),    RALT_T(KC_0),   KC_BSLS,
-        KC_TRNS,        KC_F11,         KC_F12,         KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_GRV,         KC_1,           KC_2,          KC_3,            KC_SLSH,        KC_UNDS,
+        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_F10,         KC_F12,         KC_GRV,         KC_1,           KC_2,          KC_3,            KC_SLSH,        KC_UNDS,
                                                         KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_ENT,         KC_SPC,         KC_BSPC
     ),
 
     [2] = LAYOUT_split_3x6_3(
-        KC_ESC,         KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_LBRC,        KC_RBRC,        KC_TRNS,        KC_TRNS,        KC_TRNS,
-        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_LT,          KC_LCBR,        KC_RCBR,        KC_TRNS,        KC_TRNS,        KC_BSLS,
-        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_GT,          KC_LPRN,        KC_RPRN,        KC_TRNS,        KC_TRNS,        KC_TRNS,
+        KC_ESC,         KC_TRNS,        KC_CIRC,        KC_AMPR,        KC_PERC,        LSFT(KC_GRV),   KC_LT,          KC_LBRC,        KC_RBRC,        KC_TRNS,        KC_TRNS,        KC_TRNS,
+        KC_TRNS,        KC_ASTR,        KC_HASH,        KC_AT,          KC_EXLM,        KC_DLR,         KC_GT,          KC_LCBR,        KC_RCBR,        KC_TRNS,        KC_TRNS,        KC_TRNS,
+        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_LPRN,        KC_RPRN,        KC_TRNS,        KC_TRNS,        KC_TRNS,
                                                         KC_TRNS,        KC_TRNS,        KC_TRNS,        KC_ENT,         KC_SPC,         KC_BSPC
     ),
 
     [3] = LAYOUT_split_3x6_3(
-        KC_ESC,         KC_BRIU,        KC_WSCH,        KC_WBAK,        KC_WFWD,        KC_WREF,        KC_X,           KC_HOME,         KC_UP,         KC_END,        KC_PGUP,        KC_PPLS,
-        KC_TRNS,        LALT_T(KC_BRID),LSFT_T(KC_TRNS),LCTL_T(KC_MUTE),KC_MPLY,        KC_VOLU,        KC_C,           KC_LEFT,         KC_DOWN,       KC_RGHT,       KC_PGDN,        KC_PMNS,
-        KC_TRNS,        SGUI(KC_S),     LGUI(KC_E),     KC_MSTP,        KC_MFFD,        KC_VOLD,        KC_V,           KC_Z,            KC_Y,          KC_APP,        QK_BOOT,        KC_DEL,
+        KC_ESC,         KC_BRIU,        KC_WSCH,        KC_WBAK,        KC_WFWD,        KC_WREF,        RCTL(KC_X),     KC_HOME,         KC_UP,         KC_END,         KC_PGUP,        KC_PPLS,
+        KC_TRNS,        LALT_T(KC_BRID),LSFT_T(KC_TRNS),LCTL_T(KC_MUTE),KC_MPLY,        KC_VOLU,        RCTL(KC_C),     KC_LEFT,         KC_DOWN,       KC_RGHT,        KC_PGDN,        KC_PMNS,
+        KC_TRNS,        SGUI(KC_S),     LGUI(KC_E),     KC_MSTP,        KC_MFFD,        KC_VOLD,        RCTL(KC_V),     RCTL(KC_Z),      RCTL(KC_Y),    KC_APP,         KC_INS,          KC_DEL,
                                                         KC_TRNS,        LCA_T(KC_TRNS), C_S_T(KC_TRNS), KC_ENT,         KC_SPC,          KC_BSPC
     ),
 
     [4] = LAYOUT_split_3x6_3(
-        KC_NO,          KC_NO,           KC_NO,           KC_NO,        KC_NO,          KC_NO,            KC_NO,        KC_NO,            KC_NO,            KC_NO,            KC_NO,        KC_NO,
-        KC_NO,          UM(POOP),        LSFT_T(UM(CRY)), UM(WINK),     UM(FISTBOX),    UM(EXPLODE),      KC_NO,        UP(DESAE, DEBAE), UP(DESOE, DEBOE), UP(DESUE, DEBUE), KC_E,         KC_NO,
-        KC_NO,          KC_NO,           KC_NO,           KC_NO,        KC_NO,          UM(SMILE),        KC_NO,        KC_NO,            KC_NO,            KC_NO,            KC_NO,        KC_NO,
-                                                          KC_F12,       KC_NO,          KC_TAB,           KC_ENT,       KC_SPC,           KC_BSPC
+        UM(HEARNOEVIL),         UM(SKULL),       UM(ASTONISHED),  UM(HEART),    UM(ROBOT),      UM(HEXPLODE),     KC_NO,        DEBAE,            DEBOE,            DEBUE,            KC_NO,        KC_NO,
+        UM(SEENOEVIL),          UM(POOP),        UM(CRY),         UM(WINK),     UM(FISTBOX),    UM(EXPLODE),      KC_NO,        UP(DESAE, DEBAE), UP(DESOE, DEBOE), UP(DESUE, DEBUE), KC_E,         KC_NO,
+        UM(SPEAKNOEVIL),        KC_LSFT,         UM(FRIES),       UM(BUG),      UM(GRIMACE),    UM(SMILE),        KC_NO,        KC_NO,            KC_NO,            KC_NO,            KC_NO,        KC_NO,
+                                                                  KC_F12,       KC_NO,          KC_TAB,           KC_ENT,       KC_SPC,           KC_BSPC
     )
 };
